10166625ac7c32fd8913ce54b0e8777e88099428
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-05 11:16:20 -0800

    display correct number of calls, validation for start and end year input fields

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 
-
 var startYear;
 var endYear;
 
@@ -8,6 +7,7 @@ var searchTerm;
 var queryURL;
 var beginDate;
 var endDate;
+var recordNum;
 
 $("#search").on("click", function () {
 
@@ -18,19 +18,20 @@ $("#search").on("click", function () {
     endYear = parseInt($("#endYear").val());
 
     if ((startYear > 1960) && (startYear < 2020)) {
-        beginDate = startYear + "0101";
+        beginDate = "&begin_date=" + startYear + "0101";
     } else {
         beginDate = "";
     }
 
     if ((endYear > 1960) && (endYear < 2020) && (endYear > startYear)) {
-        endDate = endYear + "1231";
+        endDate = "&end_date=" + endYear + "1231";
     } else {
-        elseDate = "";
+        endDate = "";
     }
 
+    recordNum = parseInt($('#inputGroupSelect01 option:selected').val());
 
-    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=5rQbNRg31B5QOgvxIM8sjGNjQvsAIizt";
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + beginDate + endDate + "&api-key=5rQbNRg31B5QOgvxIM8sjGNjQvsAIizt";
 
     $.ajax({
         url: queryURL,
@@ -41,27 +42,22 @@ $("#search").on("click", function () {
 
         var results = response.response.docs;
 
-        console.log(results);
+        for (var i = 0; i < recordNum; i++) {
+            var articleTitle = results[i].headline.main;
+            var articleAuthor = results[i].byline.original;
+            var section = results[i].section_name;
+            var date = results[i].pub_date;
+            var articleLink = results[i].web_url;
 
-        var articleTitle = results[0].headline.main;
-        var articleAuthor = results[0].byline.original;
-        var section = results[0].section_name;
-        var date = results[0].pub_date;
-        var articleLink = results[0].web_url;
+            console.log(articleTitle);
+            console.log(articleAuthor);
+            console.log(section);
+            console.log(date);
+            console.log(articleLink);
+        }
 
-        console.log(articleTitle);
-        console.log(articleAuthor);
-        console.log(section);
-        console.log(date);
-        console.log(articleLink);
     });
 });
 
-
-
-
-
-
 $("#clear").on("click", function () {
-    console.log("a thing");
 });
